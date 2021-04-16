ebe91b76e55c6e995d3590e3d681d32728d5d4dd
LucyRRR <lucka.roth@gmail.com>
2021-04-16 15:15:33 +0200

    added API for forecast daily

--- src/index.js
+++ src/index.js
@@ -25,6 +25,21 @@ function formatDate(date) {
 
   return `${day} ${hours}:${minutes}`;
 }
+
+function displayForecast(response){
+  console.log(response.data.daily);
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "9561e0b8516730d7561152d7deb2d27b";
+  let apiUrl =`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayForecast);
+}
+
+
+
 function displayWeatherCondition(response) {
   document.querySelectorAll(".displayCity").forEach(function(singleElement){
     singleElement.innerHTML = response.data.name;
@@ -43,6 +58,7 @@ function displayWeatherCondition(response) {
     iconElement.setAttribute ("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute ("alt", "#description");
     celsiusTemperature = response.data.main.temp;
+    getForecast (response.data.coord);
 }
 
 function searchCity(displayCity) {
