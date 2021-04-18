3ef5e541bf98ad48a48ac97ce668f2846ee90684
LucyRRR <lucka.roth@gmail.com>
2021-04-18 17:56:30 +0200

    connected API for nextDays (temperature, icon)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-//Challenge 1
-
 
 function formatDate(date) {
   let hours = date.getHours();
@@ -26,9 +24,48 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast(response){
-  console.log(response.data.daily);
+function formatDay (updateDate) {
+  let date = new Date (updateDate * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days [day];
 }
+
+function displayForecast(response){
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#nextDays");
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 
 function getForecast(coordinates) {
   console.log(coordinates);
